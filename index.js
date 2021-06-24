e2665318f99178add873764953c9b516ece0b740
Jacob <lrosentzveig@brainstation.io>
2021-06-24 10:06:40 -0700

    Added another Dad joke

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 console.log('hello! Macs is just scam spelt backwards, hello! Jon WAS HERE Also MUAHAHAHA');
 console.log(`I'm afraid for the calendar. Its days are numbered.`);
+console.log(`Singing in the shower is fun until you get soap in your mouth. Then it's a soap opera.`);
